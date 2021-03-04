754db355ccbc3d31369d82e417d22d1edaa0b081
martina.vit <martina.vit@cern.ch>
2021-03-04 12:09:27 +0100

    compile

--- compile.py
+++ compile.py
@@ -1,9 +1,29 @@
 import os, shutil
 import subprocess
+import sys
 
 
 
-os.system("pdflatex thesis.tex")
+def pdfLatex( file_name ):
+    command = 'pdflatex -synctex=1 -interaction=nonstopmode'
+    os.system( command + ' ' + file_name )
+
+
+def bibTex( file_name ):
+    base_name = os.path.splitext( file_name )[0]
+    os.system( 'bibtex {}.aux'.format( base_name ) )
+
+
+
+if __name__ == '__main__':
+    input_file = os.sys.argv[1]
+    pdfLatex( input_file )
+    bibTex( input_file )
+    pdfLatex( input_file )
+    pdfLatex( input_file )
+
+
+
 
 subprocess.call("mv %s %s" % ('*.aux', 'output_file/'), shell=True)
 subprocess.call("mv %s %s" % ('*.lof', 'output_file/'), shell=True)
@@ -11,4 +31,9 @@ subprocess.call("mv %s %s" % ('*.log', 'output_file/'), shell=True)
 subprocess.call("mv %s %s" % ('*.lot', 'output_file/'), shell=True)
 subprocess.call("mv %s %s" % ('*.out', 'output_file/'), shell=True)
 subprocess.call("mv %s %s" % ('*.xml', 'output_file/'), shell=True)
+subprocess.call("mv %s %s" % ('*.blg', 'output_file/'), shell=True)
+subprocess.call("mv %s %s" % ('*.bbl', 'output_file/'), shell=True)
 subprocess.call("mv %s %s" % ('*.toc', 'output_file/'), shell=True)
+subprocess.call("mv %s %s" % ('thesis.synctex.gz', 'output_file/'), shell=True)
+
+
