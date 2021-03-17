f7584e5d953d1e14ce1bc5ee4ac2ed303874c7a4
martina.vit <martina.vit@cern.ch>
2021-03-17 22:41:51 +0100

    firstpart c3

--- compile.py
+++ compile.py
@@ -25,16 +25,16 @@ if __name__ == '__main__':
 
 
 
-subprocess.call("mv %s %s" % ('*.aux', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.lof', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.log', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.lot', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.out', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.xml', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.blg', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.bbl', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('*.toc', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('thesis.synctex.gz', 'output_file/'), shell=True)
-subprocess.call("mv %s %s" % ('Chapters/*.aux', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.aux', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.lof', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.log', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.lot', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.out', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.xml', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.blg', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.bbl', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('*.toc', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('thesis.synctex.gz', 'output_file/'), shell=True)
+###subprocess.call("mv %s %s" % ('Chapters/*.aux', 'output_file/'), shell=True)
 
 
